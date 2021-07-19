63eae55df2ac6417ed1852c846e35652aff22da1
leandredb75 <leandredibipro@gmail.com>
2021-07-19 21:31:32 +0200

    Class Facebook done

--- oauth-client/facebook/oauthfacebook.php
+++ oauth-client/facebook/oauthfacebook.php
@@ -22,4 +22,26 @@
             ."&scope=email&state=dsdsfsfds";
             return $link;
         }
+
+        public function getFbUser($params){
+            $result = file_get_contents(FB_GRAPH . "/oauth/access_token?"
+                . "redirect_uri=https://localhost/fb-success"
+                . "&client_id=" . $this->_client_id
+                . "&client_secret=" . $this->_client_secret
+                . "&" . http_build_query($params));
+                
+            $token = json_decode($result, true)["access_token"];
+            // GET USER by TOKEN
+            $context = stream_context_create([
+                'http' => [
+                    'method' => "GET",
+                    'header' => "Authorization: Bearer " . $token
+                ]
+            ]);
+            $result = file_get_contents(FB_GRAPH. "/me", false, $context);
+            $user = json_decode($result, true);
+            var_dump($user);
+        
+            echo '<h1>Welcome '. $user["name"] . '</h1>';
+        }
     }
--- oauth-client/index.php
+++ oauth-client/index.php
@@ -1,8 +1,6 @@
 <?php
 const CLIENT_ID = "client_606c5bfe886e14.91787997";
 const CLIENT_SECRET = "2ce690b11c94aca36d9ec493d9121f9dbd5c96a5";
-const FBCLIENT_ID = "2960039270934872";
-const FBCLIENT_SECRET = "edc0f660fdc73de629915a8e9a31c175";
 
 session_start();
 
@@ -37,41 +35,6 @@ function getUser($params)
 
 }
 
-function getFbUser($params)
-{
-    $result = file_get_contents("https://graph.facebook.com/oauth/access_token?"
-        . "redirect_uri=https://localhost/fb-success"
-        . "&client_id=" . FBCLIENT_ID
-        . "&client_secret=" . FBCLIENT_SECRET
-        . "&" . http_build_query($params));
-    $token = json_decode($result, true)["access_token"];
-    // GET USER by TOKEN
-    $context = stream_context_create([
-        'http' => [
-            'method' => "GET",
-            'header' => "Authorization: Bearer " . $token
-        ]
-    ]);
-    $result = file_get_contents("https://graph.facebook.com/me", false, $context);
-    $user = json_decode($result, true);
-    var_dump($user);
-
-    echo '<h1>Welcome '. $user["name"] . '</h1>';
-}
-
-/**
- * AUTH_CODE WORKFLOW
- *  => Get CODE
- *  => EXCHANGE CODE => TOKEN
- *  => GET USER by TOKEN
- */
-/**
- * PASSWORD WORKFLOW
- * => GET USERNAME/PASSWORD (form)
- * => EXHANGE U/P => TOKEN
- * => GET USER by TOKEN
- */
-
 $route = strtok($_SERVER['REQUEST_URI'], '?');
 switch ($route) {
     case '/auth-code':
@@ -97,7 +60,7 @@ switch ($route) {
         // GET CODE
         ["code" => $code, "state" => $state] = $_GET;
         // ECHANGE CODE => TOKEN
-        getFbUser([
+        $facebook->getFbUser([
             "grant_type" => "authorization_code",
             "code" => $code
         ]);
@@ -141,31 +104,3 @@ switch ($route) {
         echo 'not_found leandre';
         break;
 }
-
-
-
-
-//$sdk = new OauthSDK([
-//    "facebook" => [
-//        "app_id",
-//        "app_secret"
-//    ],
-//    "oauth-server" => [
-//        "app_id",
-//        "app_secret"
-//    ]
-//    ]);
-//
-//$sdk->getLinks() => [
-//    "facebook" => "https://",
-//    "oauth-server" => "http://localhost:8081/auth"
-//]
-//
-//$token = $sdk->handleCallback();
-//$sdk->getUser();
-// return [
-//     "firstname"=>$facebookUSer["firstname"],
-//     "lastname"=>$facebookUSer["lastname"],
-//     "email"=>$facebookUSer["email"],
-//     "phone" =>$facebookUSer["phone_number"]
-// ];
